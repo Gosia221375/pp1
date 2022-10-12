066cf70192aa72c157b3ef46f280df46a93cb416
s-013-11 <s-013-11@gumiforest.ae.krakow.pl>
2022-10-12 11:12:11 +0200

    pp1

---
+++ 02-TypesAndVariables/Task10.py
@@ -0,0 +1,6 @@
+x=input("Enter first number: ")
+y=input("Enter second number: ")
+
+x=int(x)
+y=int(y)
+print(f"{x}+{y} is: {x+y}")
---
+++ 02-TypesAndVariables/Task11.py
@@ -0,0 +1,8 @@
+name = input("Your name is: ")
+age = input("Your age is: ")
+height = input("Your height is:")
+
+age = str(age)
+height = str(height)
+
+print(f"My name is {name}, I'm {age} years old, and my height is {height} cm.")
